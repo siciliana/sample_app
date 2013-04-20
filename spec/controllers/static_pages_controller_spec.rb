4afc5fd49800df6539a606ce1de0432355253ebc
siciliana <siciliana@gmail.com>
2013-04-19 18:25:09 -0700

    Finish layout and routes

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe StaticPagesController do
 
-  describe "GET '...'" do
+  describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
