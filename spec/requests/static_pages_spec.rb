bdf2f096126f1f142ccb288d15f62ed4dd5f2273
siciliana <siciliana@gmail.com>
2013-04-19 22:43:08 -0700

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,3 +1,4 @@
+  
 require 'spec_helper'
 
 describe "Static pages" do
@@ -6,11 +7,25 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
+    
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }  
+    it { should_not have_selector('title', text: '| Home') }
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
-  end
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
 
   describe "Help page" do
     before { visit help_path }
@@ -32,4 +47,6 @@ describe "Static pages" do
     it { should have_selector('h1',    text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
   end
+end
+end
 end
